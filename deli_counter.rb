b74fc9963ce6b64099082d39a3b07db887ab30c1
Liz Laffitte <liz.kossakowski@gmail.com>
2019-10-14 17:48:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,30 +1,30 @@
 # Write your code here.
 def line(katz_deli)
-  if katz_deli.size < 1 
-    puts "The line is currently empty."
-  else 
-    line_info = "The line is currently:"
-    katz_deli.each_with_index do |customer, index|
-      position = index + 1 
-      customer_info = " #{position}. #{customer}"
-      line_info << customer_info
-    end
-    puts line_info
-  end
-end 
+	the_line = "The line is currently"
+	if katz_deli.length < 1
+		the_line << " empty."
+	else
+		the_line << ":"
+		katz_deli.each_with_index do |customer, index|
+			position = index + 1
+			the_line << " #{position}. #{customer}"
+		end
+	end
+	puts the_line
+end
 
 def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  position = katz_deli.find_index(name) + 1
-  puts "Welcome, #{name}. You are number #{position} in line."
-end 
+	katz_deli << name
+	position = (katz_deli.index(name)) + 1
+	puts "Welcome, #{name}. You are number #{position} in line."
+end
 
 def now_serving(katz_deli)
-  if katz_deli.size < 1 
-    puts "There is nobody waiting to be served!"
-  else 
-    current_customer = katz_deli[0]
-    puts "Currently serving #{current_customer}."
-    katz_deli.shift
-  end
-end 
+	if katz_deli.length < 1
+		puts "There is nobody waiting to be served!"
+	else
+		next_customer = katz_deli.shift
+		now_serving = "Currently serving #{next_customer}."
+		puts now_serving
+	end
+end
